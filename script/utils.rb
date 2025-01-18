59f8a9d7b063f6319f584426184a0eaba09c4803
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-01-18 23:04:37 +0200

    Initial commit

---
+++ script/utils.rb
@@ -0,0 +1,54 @@
+r = RestClient.get("http://telematics.oasa.gr/api/?act=webGetLines")
+lines = JSON.parse(r.body)
+
+lines
+
+r = RestClient.get("http://telematics.oasa.gr/api/?act=getRoutesForLine&p1=#{line["LineCode"]}")
+route = JSON.parse(r.body).first
+
+# r = RestClient.get("http://telematics.oasa.gr/api/?act=webGetRoutesDetailsAndStops&p1=#{route["route_code"]}")
+# stopsJSON.parse(r.body).first
+
+
+
+r = RestClient.get("http://telematics.oasa.gr/api/?act=webGetStops&p1=#{route["route_code"]}")
+stops = JSON.parse(r.body)
+
+r = RestClient.get("http://telematics.oasa.gr/api/?act=getBusLocation&p1=#{route["route_code"]}")
+bus_locations = JSON.parse(r.body)
+
+r = RestClient.get("http://telematics.oasa.gr/api/?act=getStopArrivals&p1=#{stop["StopCode"]}")
+stop_arrivals = JSON.parse(r.body)
+
+r = RestClient.get("http://telematics.oasa.gr/api/?act=getBusLocation&p1=#{route["route_code"]}")
+bus_locations = JSON.parse(r.body)
+
+
+
+
+
+
+
+
+
+
+
+
+stop_codes = []
+r = RestClient.get("http://telematics.oasa.gr/api/?act=webGetLines")
+lines = JSON.parse(r.body)
+
+lines.each do |line|
+  puts line["LineCode"]
+  r = RestClient.get("http://telematics.oasa.gr/api/?act=getRoutesForLine&p1=#{line["LineCode"]}")
+  routes = JSON.parse(r.body)
+
+  routes.each do |route|
+    puts "  #{route["route_code"]}"
+    r = RestClient.get("http://telematics.oasa.gr/api/?act=webGetStops&p1=#{route["route_code"]}")
+    stops = JSON.parse(r.body)
+
+    stop_codes << stops.map { |stop| stop["StopCode"] }
+    stop_codes.uniq!
+  end
+end
