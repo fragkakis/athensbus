8eb410ffc41bf88be273ffb2ff4358b84e94592d
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-02-02 22:58:08 +0200

    Fixed tests

--- test/jobs/sync_stops_job_test.rb
+++ test/jobs/sync_stops_job_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SyncStopsJobTest < ActiveJob::TestCase
   test "perform" do
-    assert_enqueued_with(job: SyncStopsBatchJob, args: [Stop.minimum(:id), Stop.maximum(:id)]) do
+    assert_enqueued_with(job: SyncStopsBatchJob, args: [ Stop.minimum(:id), Stop.maximum(:id) ]) do
       SyncStopsJob.perform_now(100)
     end
   end
