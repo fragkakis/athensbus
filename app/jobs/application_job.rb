e1d66add06395c787b29f7b1a52f471bc7cd7556
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-03-13 19:18:05 +0200

    Error reporting for solid queue jobs

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,14 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+  rescue_from(Exception) do |exception|
+    context = {
+      error_class: self.class.name,
+      args: self.arguments,
+      scheduled_at: self.scheduled_at,
+      job_id: self.job_id
+    }
+    Honeybadger.notify(exception, context:)
+    raise exception
+  end
 end
