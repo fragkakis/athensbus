d29217abee310389049308bb4f4e9bbef89e9f8e
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-10-01 20:41:31 +0300

    Added direction method to route

--- app/models/route.rb
+++ app/models/route.rb
@@ -10,4 +10,12 @@ class Route < ApplicationRecord
     r = RestClient.get("http://telematics.oasa.gr/api/?act=getBusLocation&p1=#{code}")
     JSON.parse(r.body).presence || []
   end
+
+  def direction
+    sanitized_route_desc = description.first(10).gsub(/[^[:word:]]/, "")
+    sanitized_line_desc = line.description.first(10).gsub(/[^[:word:]]/, "")
+    return "come" if sanitized_route_desc.include?(sanitized_line_desc) || sanitized_route_desc.include?(sanitized_line_desc)
+    return "come" unless line.routes.where.not(id: self.id).exists?
+    "go"
+  end
 end
--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class RouteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "direction" do
+    route1 = routes(:x93_route_1)
+    assert_equal "come", route1.direction
+
+    route2 = routes(:x93_route_2)
+    assert_equal "go", route2.direction
+
+    route2.update_columns(description: "ΜΑΡΟΥΣΙ - Ν. ΙΩΝΙΑ - ΠΟΛΥΤΕΧΝΕΙΟ [ΕΝΑΛΛΑΚΤΙΚΗ ΛΟΓΩ ΑΓΩΝΑ - ΟΑΚΑ]")
+    route2.line.update_columns(description: "ΠΟΛΥΤΕΧΝΕΙΟ - Ν. ΙΩΝΙΑ - ΜΑΡΟΥΣΙ (ΕΝΑΛΛΑΚΤΙΚΗ ΛΟΓΩ ΑΓΩΝΑ-ΟΑΚΑ)")
+    assert_equal "go", route2.direction
+  end
 end
