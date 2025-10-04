145f590379652aadd2854dd0cdba7e7c277d45c3
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-10-05 00:45:20 +0300

    removed obsolete code

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,11 +1,3 @@
 class Line < ApplicationRecord
   has_many :routes
-
-  def itinerary
-    r = RestClient::Request.execute(
-      method: :get,
-      url: "http://telematics.oasa.gr/api/?act=getDailySchedule&line_code=#{code}",
-      timeout: 5)
-    JSON.parse(r.body).presence || []
-  end
 end
