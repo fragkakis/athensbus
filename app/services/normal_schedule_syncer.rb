2a5da1043d7149c194d6820278f3e1659b0ae18a
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-10-06 18:21:28 +0300

    Added handling for ΚΑΘΗΜΕΡΙΝΗ

--- app/services/normal_schedule_syncer.rb
+++ app/services/normal_schedule_syncer.rb
@@ -20,18 +20,28 @@ class NormalScheduleSyncer < BaseScheduleSyncer
 
     sunday_code = types_of_schedule.find{|e| e["sdc_descr"].include?("ΚΥΡΙΑΚΗ")}.try("[]", "sdc_code")
     saturday_code = types_of_schedule.find{|e| e["sdc_descr"].include?("ΣΑΒΒΑΤΟ")}.try("[]", "sdc_code")
-    weekday_code = types_of_schedule.find{|e| e["sdc_descr"].include?("ΚΑΘΗΜΕΡΙΝΗ") || e["sdc_descr"].include?("ΟΛΕΣ")}.try("[]", "sdc_code")
+    friday_code = types_of_schedule.find{|e| e["sdc_descr"].include?("ΠΑΡΑΣΚΕΥΗ")}.try("[]", "sdc_code")
+    weekday_code = types_of_schedule.find do |e|
+      e["sdc_descr"].include?("ΚΑΘΗΜΕΡΙΝΗ") ||
+        e["sdc_descr"].include?("ΚΑΘΗΜΕΡΙΝH") || # contains an H in english, not a duplicate of the above
+        e["sdc_descr"].include?("ΟΛΕΣ") ||
+        e["sdc_descr"].include?("ΔΕΥΤΕΡΑ -")
+    end.try("[]", "sdc_code")
 
     today_code = if Date.current.sunday?
                    sunday_code || weekday_code
                  elsif Date.current.saturday?
                    saturday_code || weekday_code
+                 elsif Date.current.friday?
+                   friday_code || weekday_code
                  else
                    weekday_code
                  end
+
+    url = "http://telematics.oasa.gr/api/?act=getSchedLines&p1=#{CGI.escape(line.line_id)}&p2=#{today_code}&p3=#{line.code}"
     r = RestClient::Request.execute(
       method: :get,
-      url: "http://telematics.oasa.gr/api/?act=getSchedLines&p1=#{CGI.escape(line.line_id)}&p2=#{today_code}&p3=#{line.code}",
+      url: url,
       timeout: 5)
     normal_schedule = JSON.parse(r.body).presence || []
 
