90611c34fc431a32dc2a18eb1a672913355d6810
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-10-13 00:41:07 +0300

    Generating reports for yesterday

--- app/jobs/route_daily_reports/generate_reports_job.rb
+++ app/jobs/route_daily_reports/generate_reports_job.rb
@@ -2,7 +2,7 @@ module RouteDailyReports
   class GenerateReportsJob < ApplicationJob
     queue_as :default
 
-    def perform(date = Date.current)
+    def perform(date = Date.yesterday)
       Rails.benchmark("Scheduling daily report generation jobs for all routes for date #{date}") do
         jobs = Route.order(:id).ids.map do |route_id|
           GenerateReportJob.new(route_id, date).set(priority: 2)
