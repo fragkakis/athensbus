3bb46db98ff3d8315ffb1d149593f5412219b655
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-10-13 13:34:01 +0300

    Setting date

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,4 +1,5 @@
 class LinesController < ApplicationController
+  before_action :set_date
   before_action :set_lines
 
   def index
@@ -10,15 +11,15 @@ class LinesController < ApplicationController
       @date_min = Arrival.minimum(:created_at).to_date.to_s
       @date_max = Arrival.maximum(:created_at).to_date.to_s
       arrivals = @route.arrivals.
-        at_date(date).
+        at_date(@date).
         includes(:vehicle, :stop)
       @trips = TripExtractor.process(@route, arrivals)
       TripTimestampSanitizer.process(@trips)
       @data = transform_to_data(@trips)
       @all_stops = @route.stops.map.with_index(1) { |stop, index| { name: stop.description, position: index } }
 
-      @daily_schedule = @route.schedules.daily.find_by(date: date)&.departure_times || []
-      @normal_schedule = @route.schedules.normal.find_by(date: date)&.departure_times || []
+      @daily_schedule = @route.schedules.daily.find_by(date: @date)&.departure_times || []
+      @normal_schedule = @route.schedules.normal.find_by(date: @date)&.departure_times || []
       @dropped_trips = @normal_schedule - @daily_schedule
       @executed_trips_count = TripsCalculator.process(@route, arrivals).values.first || 0
     end
@@ -38,20 +39,23 @@ class LinesController < ApplicationController
 
   def set_lines
     # for the dropdown
-    @lines = if Rails.env.development? || date.blank?
+    @lines = if Rails.env.development?
                Line.all.order("line_id")
              else
                line_ids = Arrival.
                  joins(:route).
-                 where("arrivals.created_at between ? and ?", date.beginning_of_day, date.end_of_day).
+                 where("arrivals.created_at between ? and ?", @date.beginning_of_day, @date.end_of_day).
                  pluck(Arel.sql("distinct routes.line_id"))
                Line.where(id: line_ids).order(:line_id)
              end
   end
 
-  def date
-    return Date.current unless params[:date].present?
-    @date ||= Date.parse(params[:date])
+  def set_date
+    if params[:date].present?
+      @date = Date.parse(params[:date])
+    else
+      @date = Date.yesterday
+    end
   end
 
   def transform_to_data(trips)
--- test/controllers/lines_controller_test.rb
+++ test/controllers/lines_controller_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class LinesControllerTest < ActionDispatch::IntegrationTest
   test "index" do
-    line = lines(:x97)
-    get lines_url(date: Date.current, line_id: line.line_id)
+    line = lines(:x93)
+    get lines_url(date: Date.current, line_code: line.code)
 
     assert_response :ok
   end
