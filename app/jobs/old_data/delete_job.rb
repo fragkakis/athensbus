a5bed43138934d73e43b3b4bb75d877d5a74378b
Markos Fragkakis <markos.fragkakis@gmail.com>
2025-10-18 12:26:16 +0300

    Updated old data deletion

--- app/jobs/old_data/delete_job.rb
+++ app/jobs/old_data/delete_job.rb
@@ -4,7 +4,7 @@ module OldData
     DATA_RETENTION_DAYS = 10
     def perform
       deletion_threshold = DATA_RETENTION_DAYS.days.ago.beginning_of_day
-      Schedule.where("date < ?", (DATA_RETENTION_DAYS + 1).days.ago).delete_all
+      Schedule.where("date < ?", (DATA_RETENTION_DAYS).days.ago).delete_all
       Arrival.where("created_at < ?", deletion_threshold).delete_all
     end
   end
