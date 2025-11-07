a3e2a1be7b9a7fb5246f3b4c6a382e1f4af32c02
Markos Fragkakis <markos.fragkakis@numan.com>
2025-11-08 00:24:31 +0200

    ThessBus.info groundwork

---
+++ config/initializers/city.rb
@@ -0,0 +1,12 @@
+def athens?
+  ENV.fetch("CITY_NAME", "athens") == "athens"
+end
+
+def thessaloniki?
+  ENV.fetch("CITY_NAME", "athens") == "thessaloniki"
+end
+
+CITY_NAME = athens? ? "Αθήνα" : "Θεσσαλονίκη"
+TELEMATICS_BASE_URL = athens? ? "http://telematics.oasa.gr" : "http://telematics.oasth.gr"
+APP_NAME = athens? ? "AthensBus.info" : "ThessBus.info"
+APP_NAME_NO_DOMAIN = athens? ? "Athens Bus" : "Thess Bus"
